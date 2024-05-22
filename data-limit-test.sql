1269b0ea546e3e0b6ac36196cb9bd685e1165453
bisbat <110973307+bisbat@users.noreply.github.com>
2024-05-22 23:50:25 +0700

    Create data-limit-test.sql

---
+++ data-limit-test.sql
@@ -0,0 +1,49 @@
+-- No Status tasks
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (7, 'NS02', 1);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (8, 'NS03', 1);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (9, 'NS04', 1);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (10, 'NS05', 1);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (11, 'NS06', 1);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (12, 'NS07', 1);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (13, 'NS08', 1);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (14, 'NS09', 1);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (15, 'NS10', 1);
+
+-- Done tasks
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (16, 'DO02', 7);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (17, 'DO03', 7);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (18, 'DO04', 7);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (19, 'DO05', 7);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (20, 'DO06', 7);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (21, 'DO07', 7);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (22, 'DO08', 7);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (23, 'DO09', 7);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (24, 'DO10', 7);
+
+-- In Progress tasks
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (25, 'IP03', 3);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (26, 'IP04', 3);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (27, 'IP05', 3);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (28, 'IP06', 3);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (29, 'IP07', 3);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (30, 'IP08', 3);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (31, 'IP09', 3);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (32, 'IP10', 3);
+
+-- To Do tasks
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (33, 'TD03', 2);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (34, 'TD04', 2);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (35, 'TD05', 2);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (36, 'TD06', 2);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (37, 'TD07', 2);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (38, 'TD08', 2);
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (39, 'TD09', 2);
+
+-- Reviewing task
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (40, 'RE01', 4);
+
+-- Testing task
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (41, 'TE01', 5);
+
+-- Waiting task
+INSERT INTO task (taskId, taskTitle, statusId) VALUES (42, 'WA01', 6);
